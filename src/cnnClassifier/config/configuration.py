e5cd954461a9a0ddb4cdb1ddebabcaba4785971d
aditya <adiphalke572@gmail.com3>
2025-04-14 15:21:30 +0530

    model_eval

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -5,6 +5,7 @@ from src.cnnClassifier.entity.config import PrepareBaseModelConfig
 import tensorflow as tf
 from src.cnnClassifier.entity.config import TrainingConfig
 import os
+from src.cnnClassifier.entity.config import EvaluationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -72,3 +73,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/adiphalke572/my-first-repo.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
